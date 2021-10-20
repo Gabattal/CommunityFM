d575376a2b5a9e38e1470d4e8745db6295195341
Gaboubou <gab-attal@hotmail.fr>
2021-10-20 11:15:24 +0200

    setup gitignore

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,7 +1,6 @@
 <template>
-  <div id="app">
+  <div id="app" style="width:100dbackground-color: #2c3e50 ">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
